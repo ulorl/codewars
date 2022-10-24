b18b8e5168b456641467216a94f50cc00f7f6222
ulorl <ul_orlova@mail.ru>
2022-10-24 13:29:19 +0300

    update Snail_Sort.py, add function snail(M)

--- Snail/Snail_Sort.py
+++ Snail/Snail_Sort.py
@@ -12,22 +12,27 @@ M = [[1, 2, 3, 4, 5, 6],
 [18, 31, 36, 35, 26, 9], 
 [17, 30, 29, 28, 27, 10], 
 [16, 15, 14, 13, 12, 11]]
-K = []
-while M != []:
-    n = len(M)
-    K.append(M.pop(0))
-    k = [row[n-1] for row in M]
-    K.append(k)
-    for i in M:
-        i.pop(-1)  
-    if M != []:
-        k = M[-1][::-1]
-        K.append(k)
-        M.pop(-1)
-        k = [row[0] for row in M]
-        K.append(k[::-1])
-        for i in M:
-            i.pop(0)  
 
-K = sum(K, [])
-print(K)
+def snail(M):
+    K = []
+    while M != []:
+        n = len(M)
+        K.append(M.pop(0))
+        k = [row[n-1] for row in M]
+        K.append(k)
+        for i in M:
+            i.pop(-1)  
+        if M != []:
+            k = M[-1][::-1]
+            K.append(k)
+            M.pop(-1)
+            k = [row[0] for row in M]
+            K.append(k[::-1])
+            for i in M:
+                i.pop(0)  
+
+    K = sum(K, [])
+        
+    return K
+
+print(snail(M))
